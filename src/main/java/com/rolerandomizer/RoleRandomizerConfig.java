3407d73524072b7a190982c8e43648b04155ebfd
Keano Porcaro <keano.porcaro@gmail.com>
2021-02-17 20:18:46 +1100

    Format RoleRandomizer to conform to code style

--- src/main/java/com/rolerandomizer/RoleRandomizerConfig.java
+++ src/main/java/com/rolerandomizer/RoleRandomizerConfig.java
@@ -9,47 +9,11 @@ public interface RoleRandomizerConfig extends Config
 {
 	@ConfigItem(
 		keyName = "player1Name",
-		name = "Player 1",
+		name = "Current team",
 		description = "Player in BA team"
 	)
-	default String player1()
+	default String teamOrder()
 	{
-		return "text1";
+		return "";
 	}
-
-	@ConfigItem(
-			keyName = "Main Attack",
-			name = "Main Attack?",
-			description = "Add preference of main attack",
-			position = 2
-	)
-	default boolean mainAttackBoolean() { return false; }
-
-	@ConfigItem(
-			keyName = "player2Name",
-			name = "Player 2",
-			description = "Player in BA team"
-	)
-	default String player2() { return "text2"; }
-
-	@ConfigItem(
-			keyName = "player3Name",
-			name = "Player 4",
-			description = "Player in BA team"
-	)
-	default String player3() { return "text3"; }
-
-	@ConfigItem(
-			keyName = "player4Name",
-			name = "Player 4",
-			description = "Player in BA team"
-	)
-	default String player4() { return "text4"; }
-
-	@ConfigItem(
-			keyName = "player5Name",
-			name = "Player 5",
-			description = "Player in BA team"
-	)
-	default String player5() { return "text5"; }
 }
--- src/main/java/com/rolerandomizer/RoleRandomizerPlugin.java
+++ src/main/java/com/rolerandomizer/RoleRandomizerPlugin.java
@@ -5,28 +5,30 @@ import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.CommandExecuted;
+import net.runelite.client.chat.ChatMessageManager;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.ClientToolbar;
 import net.runelite.client.ui.NavigationButton;
-import net.runelite.client.util.ImageUtil;
-
-import java.awt.image.BufferedImage;
+import net.runelite.client.util.ColorUtil;
+import java.awt.*;
+import java.util.Arrays;
+import java.util.HashMap;
 
 @Slf4j
 @PluginDescriptor(
-	name = "BA Role Randomizer"
+		name = "BA Role Randomizer"
 )
 public class RoleRandomizerPlugin extends Plugin
 {
-	private static final BufferedImage ICON = ImageUtil.getResourceStreamFromClass(RoleRandomizerPlugin.class, "rolerandom.png");
-
 	@Inject
 	private Client client;
+
+	@Inject
+	private ChatMessageManager chatMessageManager;
 
 	@Inject
 	private ClientToolbar clientToolbar;
@@ -34,22 +36,23 @@ public class RoleRandomizerPlugin extends Plugin
 	@Inject
 	private RoleRandomizerConfig config;
 
-	private RoleRandomizerPanel panel;
+	private RoleRandomizer randomizer;
+
 	private NavigationButton navButton;
+
+	private String[] player1Prefs;
+	private String[] player2Prefs;
+	private String[] player3Prefs;
+	private String[] player4Prefs;
+	private String[] player5Prefs;
+	private HashMap<Integer, String> usernames;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		panel = new RoleRandomizerPanel();
-		navButton = NavigationButton.builder()
-				.tooltip("BA Role Randomizer")
-				.icon(ICON)
-				.priority(6)
-				.panel(panel)
-				.build();
-		clientToolbar.addNavigation(navButton);
+		randomizer = new RoleRandomizer();
+		usernames = new HashMap<Integer, String>();
 		log.info("Example started!");
-
 	}
 
 	@Override
@@ -59,12 +62,148 @@ public class RoleRandomizerPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	public void onCommandExecuted(CommandExecuted commandExecuted) throws Exception
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		if (commandExecuted.getCommand().equals("prefs"))
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.player1(), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", usernames.get(0) + " prefs " + Arrays.toString(player1Prefs), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", usernames.get(1) + " prefs " + Arrays.toString(player2Prefs), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", usernames.get(2) + " prefs " + Arrays.toString(player3Prefs), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", usernames.get(3) + " prefs " + Arrays.toString(player4Prefs), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", usernames.get(4) + " prefs " + Arrays.toString(player5Prefs), null);
 		}
+
+		if (commandExecuted.getCommand().equals("r"))
+		{
+			if (commandExecuted.getArguments().length == 10)
+			{
+				setUsernames(commandExecuted.getArguments());
+				collectPreferences(commandExecuted.getArguments());
+				setAllPreferences();
+				String teamRoles = generateRandom();
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", teamRoles, null);
+			}
+		}
+	}
+
+	private void setUsernames(String[] commandArgs)
+	{
+		usernames.put(0, commandArgs[0]);
+		usernames.put(1, commandArgs[2]);
+		usernames.put(2, commandArgs[4]);
+		usernames.put(3, commandArgs[6]);
+		usernames.put(4, commandArgs[8]);
+		randomizer.setUsernames(usernames);
+	}
+
+	private void collectPreferences(String[] commandArgs)
+	{
+		String regex = "[^a-zA-Z0-9]";
+		player1Prefs = new String[]{commandArgs[1].replaceAll(regex, "")};
+		player2Prefs = new String[]{commandArgs[3].replaceAll(regex, "")};
+		player3Prefs = new String[]{commandArgs[5].replaceAll(regex, "")};
+		player4Prefs = new String[]{commandArgs[7].replaceAll(regex, "")};
+		player5Prefs = new String[]{commandArgs[9].replaceAll(regex, "")};
+	}
+
+	/**
+	 * prefs should be entered into the command like so
+	 *  ::r <username> <preferences>
+	 * where preferences can be of the following:
+	 *  a 2 h c d fill
+	 */
+	private void setAllPreferences()
+	{
+		// prefs should be entered like this
+		// ::r <name> <prefs>
+		// where prefs can be a minimum of 3 of the following:
+		//  a/2/h/c/d or fill
+		// no prefs specified will be handled in another statement
+		randomizer.setPlayerOnePreferences(convertPreferences(player1Prefs));
+		randomizer.setPlayerTwoPreferences(convertPreferences(player2Prefs));
+		randomizer.setPlayerThreePreferences(convertPreferences(player3Prefs));
+		randomizer.setPlayerFourPreferences(convertPreferences(player4Prefs));
+		randomizer.setPlayerFivePreferences(convertPreferences(player5Prefs));
+	}
+
+	private String generateRandom() throws Exception
+	{
+		String[] roles = randomizer.randomize();
+		String shortFormRoles = "";
+		for (int index = 0; index < 5; index++)
+		{
+			switch(index)
+			{
+				case 0:
+				case 1:
+					shortFormRoles += ColorUtil.wrapWithColorTag(
+							roles[index]
+									.substring(0, 1)
+									.toUpperCase()
+									+roles[index]
+									.substring(1), Color.RED.darker()) + " / ";
+					break;
+				case 2:
+					shortFormRoles += ColorUtil.wrapWithColorTag(
+							roles[index]
+									.substring(0, 1)
+									.toUpperCase()
+									+roles[index]
+									.substring(1), Color.GREEN.darker().darker()) + " / ";
+					break;
+				case 3:
+					shortFormRoles += ColorUtil.wrapWithColorTag(
+							roles[index]
+									.substring(0, 1)
+									.toUpperCase()
+									+roles[index]
+									.substring(1), Color.YELLOW) + " / ";
+					break;
+				case 4:
+					shortFormRoles += ColorUtil.wrapWithColorTag(
+							roles[index]
+									.substring(0, 1)
+									.toUpperCase()
+									+roles[index]
+									.substring(1), Color.BLUE.darker());
+					break;
+			}
+		}
+		return shortFormRoles;
+	}
+
+	private int[] convertPreferences(String[] prefs)
+	{
+		int[] newPrefs = {0, 0, 0, 0, 0};
+		for (String s : prefs) {
+			if (s.equals("fill"))
+			{
+				return new int[]{1, 1, 1, 1, 1};
+			}
+			for (int i = 0; i < s.length(); i++)
+			{
+				char c = s.charAt(i);
+				switch (c) {
+					case 'a':
+						newPrefs[0] = 1;
+						newPrefs[1] = 1;
+						break;
+					case '2':
+						newPrefs[1] = 1;
+						break;
+					case 'h':
+						newPrefs[2] = 1;
+						break;
+					case 'c':
+						newPrefs[3] = 1;
+						break;
+					case 'd':
+						newPrefs[4] = 1;
+						break;
+				}
+			}
+		}
+		return newPrefs;
 	}
 
 	@Provides
