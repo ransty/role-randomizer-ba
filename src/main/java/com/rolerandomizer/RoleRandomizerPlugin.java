82f0fe45ae3d38fdb578f59f311137cf1fa8af28
Keano Porcaro <keano.porcaro@gmail.com>
2021-02-17 20:27:46 +1100

    Add required module docstring with copyright
    Reorder imports to conform to codestyle

--- src/main/java/com/rolerandomizer/RoleRandomizerPlugin.java
+++ src/main/java/com/rolerandomizer/RoleRandomizerPlugin.java
@@ -1,8 +1,35 @@
+/*
+ * Copyright (c) 2021, Keano Porcaro <keano@ransty.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.rolerandomizer;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.HashMap;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.events.CommandExecuted;
@@ -14,9 +41,6 @@ import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.ClientToolbar;
 import net.runelite.client.ui.NavigationButton;
 import net.runelite.client.util.ColorUtil;
-import java.awt.*;
-import java.util.Arrays;
-import java.util.HashMap;
 
 @Slf4j
 @PluginDescriptor(
@@ -52,12 +76,13 @@ public class RoleRandomizerPlugin extends Plugin
 	{
 		randomizer = new RoleRandomizer();
 		usernames = new HashMap<Integer, String>();
-		log.info("Example started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
+		usernames = null;
+		randomizer = null;
 		log.info("Example stopped!");
 	}
 
@@ -106,19 +131,8 @@ public class RoleRandomizerPlugin extends Plugin
 		player5Prefs = new String[]{commandArgs[9].replaceAll(regex, "")};
 	}
 
-	/**
-	 * prefs should be entered into the command like so
-	 *  ::r <username> <preferences>
-	 * where preferences can be of the following:
-	 *  a 2 h c d fill
-	 */
 	private void setAllPreferences()
 	{
-		// prefs should be entered like this
-		// ::r <name> <prefs>
-		// where prefs can be a minimum of 3 of the following:
-		//  a/2/h/c/d or fill
-		// no prefs specified will be handled in another statement
 		randomizer.setPlayerOnePreferences(convertPreferences(player1Prefs));
 		randomizer.setPlayerTwoPreferences(convertPreferences(player2Prefs));
 		randomizer.setPlayerThreePreferences(convertPreferences(player3Prefs));
